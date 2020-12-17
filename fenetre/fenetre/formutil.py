1d5934752d02d5ae88c146e29c6cc15ad6a60e9c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-12-17 17:04:09 -0500

    fenetre: fix formutil

--- fenetre/fenetre/formutil.py
+++ fenetre/fenetre/formutil.py
@@ -6,7 +6,7 @@ def form_geometry_compatible(geometry: FormGeometry, with_form: Form):
         return True
 
     for field in geometry.fields:
-        if not any(x.index_on_page == x.index and x.kind == field.kind and x.expected_label_segment in field.title for x in with_form.sub_fields):
+        if not any(x.index_on_page == field.index and x.kind == field.kind.value and x.expected_label_segment in field.title for x in with_form.sub_fields):
             return False
 
     return True
