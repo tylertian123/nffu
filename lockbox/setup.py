efd70ac7bf97986c74bff314ee382ba9e2d00c14
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-12-28 22:54:53 -0500

    ops: version bump to 0.1.2

--- lockbox/setup.py
+++ lockbox/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as fh:
 
 setup(
     name="lockbox",
-    version="0.1.1",
+    version="0.1.2",
     description="",
     packages=["lockbox"],
     install_requires=install_requires,
